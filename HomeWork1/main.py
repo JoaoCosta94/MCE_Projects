526379036745b527956cb24d5629b5300685a4b4
JoaoCosta94 <jpcc0394@gmail.com>
2015-10-16 21:26:07 +0100

    Added numerical FFT of signals

--- HomeWork1/main.py
+++ HomeWork1/main.py
@@ -1,4 +1,5 @@
 import pylab as pl
+import numpy as np
 
 def rectangle_function(x, spacing, left_limit, w, h):
     """
@@ -40,13 +41,56 @@ def cosine(x, left_limit, right_limit, spacing, w):
     x = pl.arange(left_limit, right_limit, spacing)
     return pl.cos(w * x)
 
-def e1():
+def fourier_numerical(signalsList):
 
-#   creation of original functions
-    x = pl.arange(-2, 2+0.01, 0.01)
-    rec = rectangle_function(x, 0.01, -1, 2, 1)
-    pl.plot(x, pl.cos(6*x))
-    pl.plot(x, rec)
+    """
+    This method calculates given signals FFT using numpy.fft
+    :param signalsList: List with signals array to transform
+    :return: Tuple with list of FFT of original signals and list of FFT frequencies
+    """
 
-e1()
+    signalsFFT = []
+    fftFreqs = []
+    for signal in signalsList:
+        signalsFFT.append(np.fft.fft(signal))
+        fftFreqs.append(np.fft.fftfreq(signal.size))
+
+    return (signalsFFT, fftFreqs)
+
+# Definition of general conditions of margins and time (may be changed)
+left_limit = -5
+right_limit = 5
+dt = 0.01
+t = pl.arange(left_limit, right_limit, dt)
+
+# Definition of rectangle
+a = 2 # This may be changed
+rec = rectangle_function(t, dt, -a, 2*a, 1)
+# Definition of original cosine function
+w = 4 # This may be changed
+c = pl.cos(w * t)
+
+# Plotting of original signals
+pl.figure(1)
+pl.title("Original Signals")
+pl.xlabel("time")
+pl.ylabel("f(t)")
+pl.plot(t,rec, label = "rec(t)")
+pl.plot(t, c, label = "cos(wt)")
+pl.legend()
+
+# FFT of original signals
+fft, fftFreq = fourier_numerical((rec, c))
+recFFT = np.absolute(fft[0])
+cFFT = np.absolute(fft[1])
+
+# Plotting FFTs
+pl.figure(2)
+pl.title("REC FFT")
+pl.plot(fftFreq[0], recFFT)
+pl.figure(3)
+pl.title("COS FFT")
+pl.plot(fftFreq[1], cFFT)
+
 pl.show()
+
