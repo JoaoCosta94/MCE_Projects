6f2e5524bceb39d549bd821b0130a387876db0eb
JoaoCosta94 <jpcc0394@gmail.com>
2015-10-17 15:23:07 +0100

    changed for mutiple a and w

--- HomeWork1/HW1_1.py
+++ HomeWork1/HW1_1.py
@@ -46,6 +46,12 @@ def fourier_numerical(signalsList, spacing):
     return (signalsFFT, fftFreqs)
 
 def recFourier(f, hw):
+    """
+    This function calculates the analytical Fourier Transform of a rectangular pulse
+    :param f:   frequencies where to calculate de fourier transform
+    :param hw:  Half width of the rectangular pulse
+    :return:    Array with fourier transform of given rectangular pulse
+    """
     return hw*np.sinc(2.0*hw*f)*2.0
 
 if __name__  == "__main__":
@@ -56,77 +62,85 @@ if __name__  == "__main__":
     dt = 0.01
     t = np.arange(left_limit, right_limit, dt)
 
-    # Definition of rectangle
-    a = 0.5 # This may be changed
-    rec = rectangle_function(t, dt, -a, 2*a, 1)
+    # Definition of rectangle width and cosine angular velocity
+    a_list = [1, 0.5, 0.1]
+    w_list = [1, 8, 8*np.pi]
 
-    # Definition of original cosine function
-    w = 1 # This may be changed
-    c = np.cos(w * t)
+    # Obtaining original signals
+    rec_list = []
+    cs_list = []
+    for i in range(len(a_list)):
+        a = a_list[i]
+        w = w_list[i]
+        rec_list.append(rectangle_function(t, dt, -a, 2*a, 1))
+        cs_list.append(np.cos(w * t))
 
-    # FFT of original signals
-    # Numerical solution
-    fftRes, fftFreq = fourier_numerical((rec, c), dt)
-
-    recFFT = abs(fftRes[0])
-    cosFFT = abs(fftRes[1])
+    # Fourier Transform of original signals
+    rec_FFT_list = []
+    cs_FFT_list  = []
+    rec_fourier_list = []
+    cs_fourier_list = []
+    for i in range(len(rec_list)):
+        # Numerical solution
+        fftRes, fftFreq = fourier_numerical((rec_list[i], cs_list[i]), dt)
+        rec_FFT_list.append(abs(fftRes[0]))
+        cs_FFT_list.append(abs(fftRes[1]))
+        # Analytical solution
+        rec_fourier_list.append((recFourier(fftFreq[0], a_list[i])))
+        # missing cosine
     freq = fftFreq[0]
 
-    # Analytical solution
-    recFourierTransform = abs(recFourier(freq, a))
-    # missing cosine
-
-    # Convolutions
-    # By numpy.convolve
-    convNP = np.convolve(rec, c, "same")
-    # By fourier transform convolution
-    conv = fftconvolve(rec, c, "same")
-
-
-    # Plotting of original signals
-    pl.figure("Original Signals")
-    pl.subplot(211)
-    pl.title("Rectangle")
-    pl.xlabel("time")
-    pl.ylabel("f(t)")
-    pl.plot(t,rec, label = "rec(t)")
-    pl.subplot(212)
-    pl.title("Cosine")
-    pl.xlabel("time")
-    pl.ylabel("f(t)")
-    pl.plot(t, c, label = "cos(wt)")
-
-    # Plotting FFTs
-    pl.figure("Fourier Transforms")
-    # Rectangular Pulse Transform
-    pl.subplot(211)
-    pl.title("Rectangular Pulse Transform")
-    pl.plot(freq, recFFT / max(recFFT), label = "FFT")
-    pl.plot(freq, recFourierTransform / max(recFourierTransform), label = "Analytical Solution")
-    pl.xlim((-50, 50))
-    pl.legend()
-    # Cosine Transform
-    pl.subplot(212)
-    pl.title("Cosine Transform")
-    pl.plot(freq, cosFFT / max(cosFFT), label = "FFT")
-    pl.xlim((-w/np.pi, w/np.pi))
-    pl.legend()
-
-    # Plotting convolutions
-    pl.figure("Convolutions")
-    pl.subplot(211)
-    pl.title("Convolution Result")
-    pl.plot(t, convNP, label = "Numpy Convolve")
-    pl.plot(t, conv, label = " FFTConvolve")
-    pl.xlabel("time")
-    pl.ylabel("f(t)")
-    pl.legend()
-    pl.subplot(212)
-    pl.title("Error")
-    pl.plot(t, abs(conv-convNP))
-    pl.xlabel("time")
-    pl.ylabel("f(t)")
-    pl.ylim((-10**(-12)), 10**(-12))
-
-    pl.show()
+    # # Convolutions
+    # # By numpy.convolve
+    # convNP = np.convolve(rec, c, "same")
+    # # By fourier transform convolution
+    # conv = fftconvolve(rec, c, "same")
+    #
+    #
+    # # Plotting of original signals
+    # pl.figure("Original Signals")
+    # pl.subplot(211)
+    # pl.title("Rectangle")
+    # pl.xlabel("time")
+    # pl.ylabel("f(t)")
+    # pl.plot(t,rec, label = "rec(t)")
+    # pl.subplot(212)
+    # pl.title("Cosine")
+    # pl.xlabel("time")
+    # pl.ylabel("f(t)")
+    # pl.plot(t, c, label = "cos(wt)")
+    #
+    # # Plotting FFTs
+    # pl.figure("Fourier Transforms")
+    # # Rectangular Pulse Transform
+    # pl.subplot(211)
+    # pl.title("Rectangular Pulse Transform")
+    # pl.plot(freq, recFFT / max(recFFT), label = "FFT")
+    # pl.plot(freq, recFourierTransform / max(recFourierTransform), label = "Analytical Solution")
+    # pl.xlim((-50, 50))
+    # pl.legend()
+    # # Cosine Transform
+    # pl.subplot(212)
+    # pl.title("Cosine Transform")
+    # pl.plot(freq, cosFFT / max(cosFFT), label = "FFT")
+    # pl.xlim((-w/np.pi, w/np.pi))
+    # pl.legend()
+    #
+    # # Plotting convolutions
+    # pl.figure("Convolutions")
+    # pl.subplot(211)
+    # pl.title("Convolution Result")
+    # pl.plot(t, convNP, label = "Numpy Convolve")
+    # pl.plot(t, conv, label = " FFTConvolve")
+    # pl.xlabel("time")
+    # pl.ylabel("f(t)")
+    # pl.legend()
+    # pl.subplot(212)
+    # pl.title("Error")
+    # pl.plot(t, abs(conv-convNP))
+    # pl.xlabel("time")
+    # pl.ylabel("f(t)")
+    # pl.ylim((-10**(-12)), 10**(-12))
+    #
+    # pl.show()
 
