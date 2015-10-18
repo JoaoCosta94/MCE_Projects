c9f40440e1b2396be17849c6c075791601bb2319
Joao Costa <jcosta@top-docs.co>
2015-10-18 19:58:40 +0100

    Finished for Added roll Added conv Started fft Added 2D plot Added time plot Added average error plot

--- HomeWork1/HW1_2.py
+++ HomeWork1/HW1_2.py
@@ -1,5 +1,8 @@
 import numpy as np
+from numpy import sin, cos, exp
+from scipy.signal import convolve2d
 import pylab as pl
+import time
 
 def f(x,y):
     """
@@ -10,14 +13,14 @@ def f(x,y):
     """
     return (np.sin(x) + np.sin(x+y) + np.sin(x*y))*np.exp(-(x*x + y*y)/sigma)
 
-def analyticalLapf(X,Y):
+def analyticalLapf(x,y, o):
     """
     This function returns de analytical laplacian of function f
     :param X:
     :param Y:
     :return:
     """
-    return "cenas"
+    return (-x**2*sin(x*y) - sin(x + y))*exp(-(x**2 + y**2)**2/o) + (-y**2*sin(x*y) - sin(x) - sin(x + y))*exp(-(x**2 + y**2)**2/o) - 8*x**2*(sin(x) + sin(x*y) + sin(x + y))*exp(-(x**2 + y**2)**2/o)/o - 8*x*(x**2 + y**2)*(y*cos(x*y) + cos(x) + cos(x + y))*exp(-(x**2 + y**2)**2/o)/o - 8*y**2*(sin(x) + sin(x*y) + sin(x + y))*exp(-(x**2 + y**2)**2/o)/o - 8*y*(x**2 + y**2)*(x*cos(x*y) + cos(x + y))*exp(-(x**2 + y**2)**2/o)/o - 8*(x**2 + y**2)*(sin(x) + sin(x*y) + sin(x + y))*exp(-(x**2 + y**2)**2/o)/o + 16*x**2*(x**2 + y**2)**2*(sin(x) + sin(x*y) + sin(x + y))*exp(-(x**2 + y**2)**2/o)/o**2 + 16*y**2*(x**2 + y**2)**2*(sin(x) + sin(x*y) + sin(x + y))*exp(-(x**2 + y**2)**2/o)/o**2
 
 def dx2For(G, h):
     """
@@ -66,17 +69,48 @@ def dy2For(G,h):
 def forMethod(G, h):
     """
     This function calculates the laplacian with a python for approach
-    :param X:
-    :param Y:
-    :param h:
-    :return:
+    :param G:   f(X,Y)
+    :param h:   Derivative step
+    :return:    laplacian of f(X,Y) using for method
     """
-    return "cenas"
+    start = time.time()
+    lap = dx2For(G,h) + dy2For(G,h)
+    return (lap, time.time()-start)
 
-def rollMethod(X,Y,h):
-    return "cenas"
+def rollMethod(G,h):
+    """
+    This function calculates the laplacian of f(X,Y) with a numpy roll for approach
+    :param G:   f(X,Y)
+    :param h:   Derivative step
+    :return:    Laplacian of f(X,Y) using numpy roll
+    """
+    start = time.time()
+    dx2 = np.roll(G, 1, 0) -2.0*G + np.roll(G, -1, 0)
+    dy2 = np.roll(G, 1, 1) -2.0*G + np.roll(G, -1, 1)
+    return ((dx2 + dy2)/(h*h), time.time()-start)
 
-def convMethod(X,Y,h):
+def convMethod(G, h):
+    """
+    This function calculates the laplacian of f(X,Y) with a computational molecule and convolution approach
+    :param G:   f(X,Y)
+    :param h:   Derivative step
+    :return:    Laplacian of f(X,Y) using a computational molecule
+    """
+    start = time.time()
+    computationalMolecule = np.array([[0.0, 1.0, 0.0],
+                                      [1.0, -4.0, 1.0],
+                                      [0.0, 1.0, 0.0]])
+    # Since on the other mehtods, on the boundaries, the neighbours were considered to be 0,
+    # here the boundary was used the default mode which is fill, and the fill value being 0 (also default)
+    return (convolve2d(G, computationalMolecule, mode = 'same')/(h*h), time.time() - start)
+
+def fftMethod(G, h):
+    """
+    This function calculates the laplacian of f(X,Y) with a fourier space laplacian approach
+    :param G:   f(X,Y)
+    :param h:   Derivative step
+    :return:    Laplacian of f(X,Y) using a FT
+    """
     return "cenas"
 
 if __name__  == "__main__":
@@ -85,17 +119,10 @@ if __name__  == "__main__":
     sigma = 3.0
     xLim = 5.0
     yLim = 5.0
-    spacing = 0.1
 
-    # Creation of the points grid to evaluate the second derivative
-    X, Y = np.mgrid[-xLim:xLim+spacing:spacing, -yLim:yLim+spacing:spacing]
-    fV = f(X, Y)
-    print fV
-    # Calculation of the Laplacian through the analytical expression
-    lapAnalytical = analyticalLapf(X,Y)
+    # Creation of list of derivative steps to evaluate (May be changed)
+    h_list = [1.0, 0.1, 0.01]
 
-    # Creation of list of derivative steps to evaluate
-    h_list = [1, 0.1, 0.01, 0.001, 0.0001]
     result_for_list = []
     time_for_list = []
     error_for_list = []
@@ -107,21 +134,106 @@ if __name__  == "__main__":
     result_conv_list = []
     time_conv_list = []
     error_conv_list = []
+
+    result_fft_list = []
+    time_fft_list = []
+    error_fft_list = []
+
+    analyticalSol_list = []
     for h in h_list:
+        print h
+        # Creation of the points grid to evaluate the second derivative
+        X, Y = np.mgrid[-xLim:xLim+h:h, -yLim:yLim+h:h]
+        # Calculation of f(X,Y)
+        fXY = f(X, Y)
+        # Calculation of the Laplacian through the analytical expression
+        lapAnalytical = analyticalLapf(X,Y, sigma)
+        analyticalSol_list.append(lapAnalytical)
+
         # Calculation through for method
-        result, time = forMethod(fV, h)
+        result, t = forMethod(fXY, h)
         result_for_list.append(result)
-        time_for_list.append(result)
-        # error_for_list.append(abs(result - lapAnalytical))
-    #
-    #     # By numpy roll
-    #     result, time = rollMethod(fV, h)
-    #     result_roll_list.append(result)
-    #     time_roll_list.append(result)
-    #     # error_roll_list.append(abs(result - lapAnalytical))
-    #
-    #     # By convolution with computational molecule
-    #     result, time = convMethod(fV, h)
-    #     result_conv_list.append(result)
-    #     time_conv_list.append(result)
-    #     # error_conv_list.append(abs(result - lapAnalytical))
+        time_for_list.append(t)
+        error_for_list.append(np.average(abs(result - lapAnalytical)).ravel())
+
+        # By numpy roll
+        result, t = rollMethod(fXY, h)
+        result_roll_list.append(result)
+        time_roll_list.append(t)
+        error_roll_list.append(np.average(abs(result - lapAnalytical)).ravel())
+
+        # By convolution with computational molecule
+        result, t = convMethod(fXY, h)
+        result_conv_list.append(result)
+        time_conv_list.append(t)
+        error_conv_list.append(np.average(abs(result - lapAnalytical)).ravel())
+
+        # # By FT laplacian
+        # result, t = fftMethod(fXY, h)
+        # result_fft_list.append(result)
+        # time_fft_list.append(t)
+        # error_fft_list.append(np.average(abs(result - lapAnalytical)).ravel())
+
+    # Plotting of results
+    for i in range(len(h_list)):
+        X, Y = np.mgrid[-xLim:xLim+h_list[i]:h_list[i], -yLim:yLim+h_list[i]:h_list[i]]
+        pl.figure("h = " + str(h_list[i]))
+        # Plot of for method results
+        pl.subplot(221)
+        pl.title("For")
+        pl.xlabel("x")
+        pl.ylabel("y")
+        pl.contourf(X, Y, result_for_list[i], levels = np.linspace(min(result_for_list[i].ravel()),max(result_for_list[i].ravel()),1000))
+        pl.colorbar()
+
+        # Plot of roll method results
+        pl.subplot(222)
+        pl.title("Numpy Roll")
+        pl.xlabel("x")
+        pl.ylabel("y")
+        pl.contourf(X, Y, result_roll_list[i], levels = np.linspace(min(result_roll_list[i].ravel()),max(result_roll_list[i].ravel()),1000))
+        pl.colorbar()
+
+        # Plot of computational molecule method results
+        pl.subplot(223)
+        pl.title("Computational Molecule")
+        pl.xlabel("x")
+        pl.ylabel("y")
+        pl.contourf(X, Y, result_conv_list[i], levels = np.linspace(min(result_conv_list[i].ravel()),max(result_conv_list[i].ravel()),1000))
+        pl.colorbar()
+
+        # # Plot of FT laplacian method result
+        # pl.subplot(224)
+        # pl.title("FT")
+        # pl.xlabel("x")
+        # pl.ylabel("y")
+        # pl.contourf(X, Y, result_fft_list[i], levels = np.linspace(min(result_fft_list[i].ravel()),max(result_fft_list[i].ravel()),1000))
+        # pl.colorbar()
+
+        # # Plot of Analytical solution
+        # pl.subplot(224)
+        # pl.title("Analytical Solution")
+        # pl.xlabel("x")
+        # pl.ylabel("y")
+        # pl.contourf(X, Y, analyticalSol_list[i], levels = np.linspace(min(analyticalSol_list[i].ravel()),max(analyticalSol_list[i].ravel()),1000))
+        # pl.colorbar()
+
+    pl.figure("Satistical analisys")
+    pl.subplot(211)
+    pl.plot(h_list, time_for_list, label = "For Method")
+    pl.plot(h_list, time_roll_list, label = "Roll Method")
+    pl.plot(h_list, time_conv_list, label = "Conv Method")
+    # pl.plot(h_list, time_fft_list, label = "For Method")
+    pl.xlabel("h")
+    pl.ylabel("Calculation Time (s)")
+    pl.legend()
+    pl.subplot(212)
+    pl.plot(h_list, error_for_list, label = "For Method")
+    pl.plot(h_list, error_roll_list, label = "Roll Method")
+    pl.plot(h_list, error_conv_list, label = "Conv Method")
+    # pl.plot(h_list, error_fft_list, label = "For Method")
+    pl.xlabel("h")
+    pl.ylabel("Average Error")
+    pl.legend()
+
+    pl.show()
