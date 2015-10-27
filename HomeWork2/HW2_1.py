2e4e2a482742cb6b7b75e23489249c0c38391895
Joao Costa <jcosta@top-docs.co>
2015-10-27 19:52:55 +0000

    Continued E1

--- HomeWork2/HW2_1.py
+++ HomeWork2/HW2_1.py
@@ -1,5 +1,15 @@
 import numpy as np
+import pylab as pl
 
+def analyticalInverse(e):
+    """
+    This function returns the inverse matrix of
+    [[1.0,     1.0],
+    [1.0+e, 1.0-e]]
+    :param e:   Matrix variables
+    :return:    Inverse matrix
+    """
+    return np.array([[e - 1.0, e + 1.0], [1.0, -1.0]], dtype =np.float32) / e
 
 def invertMatrix(M):
     """
@@ -12,7 +22,9 @@ def invertMatrix(M):
 def calculateMatrixNorm(M, manual = False):
     """
     This function calculates the norm of a matrix
-    :param M:
+    Manual mode iterates each column and sums it's elements and return the maximum sum
+    The other mode uses numpy.linalg.norm to calculate the norm of M
+    :param M:   Matrix to calculate the norm of
     :return:    the norm of a matrix
     """
     if (manual):
@@ -32,32 +44,60 @@ def calculateCondNumber(M, iM = None):
     :param iM:  Inverse matrix of m
     :return:    Desired condition number
     """
-    if iM:
+    if iM == None:
         # If inverse matrix is passed as an argument, calculates directly
-        return calculateMatrixNorm(M) * calculateMatrixNorm(iM)
+        return calculateMatrixNorm(M) * calculateMatrixNorm(invertMatrix(M))
     else:
         # If inverse matrix is not passed as argument, must first calculate the inverse
-        return calculateMatrixNorm(M) * calculateMatrixNorm(invertMatrix(M))
+        return calculateMatrixNorm(M) * calculateMatrixNorm(iM)
 
 
 if __name__ == "__main__":
     #Initial Conditions
-    e_min = 0.0 # minimum value for e
-    e_max = 1.0 # maximum value for e
-    nE = 10 # number of values of e desired
+    e_min = 10.0**(-10) # minimum value for e
+    e_max = 10.0 # maximum value for e
+    nE = 1000 # number of values of e desired
 
     # Arranging the values of e
-    # e_array = np.linspace(e_min, e_max)
-    e_array = np.array([1.0])
+    e_array = np.linspace(e_min, e_max, nE)
+    # e_array = np.array([1.0])
+    # print e_array
 
     # Iteration over e to calculate the desired matrices and values
-    condNumber_list = []
+    condNumber_list_numerical = []
+    condNumber_list_analytical = []
     for e in e_array:
+
         # Creation of matrix for respective e value
         M = 0.5 * np.array([[1.0, 1.0 + e], [1.0 + e, 1.0 - e]])
         # Calculation of inverse matrix of M
         iM = invertMatrix(M)
-        print iM
         # Calculation of condition number for matrix M
-        condNumber_list.append(calculateCondNumber(M, iM))
+        condNumber_list_numerical.append(calculateCondNumber(M, iM))
+
+        # Calculations with analytical inverse matrix
+        iM = analyticalInverse(e)
+        condNumber_list_analytical.append(calculateCondNumber(M, iM))
+
+    print '(Numerical) Condition number for e = ' + str(e_array[0])
+    print condNumber_list_numerical[0]
+
+    print '(Analytical) Condition number for e = ' + str(e_array[0])
+    print condNumber_list_analytical[0]
+
+    print 'Analytical Inverse'
+    print analyticalInverse(1.0E-10)
+
+    print 'Numerical Inverse'
+    print invertMatrix(0.5 * np.array([[1.0, 1.0 + 1.0E-10], [1.0 + e, 1.0 - 1.0E-10]]))
+
+    pl.figure("Condition number")
+    # TODO: Change epsilon hardcoded to raw string
+    pl.title("Condition number vs epsilon")
+    pl.plot(e_array, condNumber_list_numerical, label = 'N. inverse')
+    pl.plot(e_array, condNumber_list_analytical, label = 'A. inverse')
+    pl.legend()
+
+    pl.show()
+
 
