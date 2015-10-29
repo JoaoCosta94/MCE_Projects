14abc7a633d85a993f38d68e7c62e188863e0e25
JoaoCosta94 <jpcc0394@gmail.com>
2015-10-29 20:54:49 +0000

    E2 done

--- HomeWork2/HW2_2.py
+++ HomeWork2/HW2_2.py
@@ -10,6 +10,23 @@ def svdDecomp(M):
     """
     return np.linalg.svd(M)
 
+def eigenValuesAndVectors(M):
+    """
+    This function calculates the eigen values and vectors of a matrix, if possible
+    :param M:   Matrix to calculate eigen values and vectors
+    :return:    Matrix's M eigen values and vectors if they are defined, or error
+    """
+    try:
+        return np.linalg.eig(M)
+    except np.linalg.linalg.LinAlgError as error:
+        return error
+
+def assertResult(result):
+    if type(result) == tuple:
+        return "Values: \n" + str(result[0]) + '\n Vectors: \n' + str(result[1])
+    else:
+        return "Can't be displayed because LinAlgError: " + str(result)
+
 if __name__ == '__main__':
 
     global A
@@ -27,8 +44,16 @@ if __name__ == '__main__':
                   [1.0, 1.0, -1.0, 0.0],
                   [2.0, 0.0, 0.0,  1.0]])
 
+    # SVD Decomposition of matrix A
     uA, sA, vA = svdDecomp(A)
+    print "Single values of matrix A"
     print sA
+    print "Matrix A eigen values and vectores, respectively"
+    print assertResult(eigenValuesAndVectors(A))
 
+    print "######################"
     uB, sB, vB = svdDecomp(B)
+    print "Single values of matrix B"
     print sB
+    print "Matrix B eigen values and vectores, respectively"
+    print assertResult(eigenValuesAndVectors(B))
