b01163f00732f86fea03cde7d2c65e2725498274
Joao Costa <jcosta@top-docs.co>
2015-10-29 17:32:51 +0000

    Cenas

--- HomeWork2/HW2_2.py
+++ HomeWork2/HW2_2.py
@@ -1,1 +1,34 @@
-__author__ = 'JoaoCosta'
+import numpy as np
+import pylab as pl
+
+def svdDecomp(M):
+    """
+    This function calculates the single values of the matrix M
+    and the respective matrices U & V
+    :param M:   Matrix to perform SVD
+    :return:    Matrix U, Single values (S), Matrix V
+    """
+    return np.linalg.svd(M)
+
+if __name__ == '__main__':
+
+    global A
+    global B
+
+    # Initialization of matrices A & B
+    A = np.array([[1.0, 0.0, 0.0, 0.0],
+                  [0.0, 0.0, 0.0, 4.0],
+                  [0.0, 3.0, 0.0, 0.0],
+                  [0.0, 0.0, 0.0, 0.0],
+                  [2.0, 0.0, 0.0, 0.0]])
+
+    B = np.array([[1.0, 0.0, 1.0,  2.0],
+                  [0.0, -1.0, 1.0, 1.0],
+                  [1.0, 1.0, -1.0, 0.0],
+                  [2.0, 0.0, 0.0,  1.0]])
+
+    uA, sA, vA = svdDecomp(A)
+    print sA
+
+    uB, sB, vB = svdDecomp(B)
+    print sB
