438a31ca82b1742aab41e50bd31153bfc58f63a3
Joao Costa <jcosta@top-docs.co>
2015-11-27 15:19:54 +0000

    Changed paths and fixed eigen states

--- HomeWork3/HW3_1c2.py
+++ HomeWork3/HW3_1c2.py
@@ -1,10 +1,10 @@
 __author__ = 'JoaoCosta'
 
 import scipy as sp
-import pylab as pl
 from scipy import sum, linalg
 import time
 import pylab as pl
+import platform
 
 def potV(X, Y, xyF, a, b, V0):
     Z = sp.zeros(X.shape)
@@ -53,7 +53,7 @@ def H(X, Y, xyF, xyS, nm, nmIndexes, V):
     :param xyF:         External square box limit
     :param xyS:         Grid points spacing
     :param nm:          Total of states used
-    :param nmIndexes:   (n,m) indexes of the states
+    :param nmIndexes:   (n,m) indexes of the state basis
     :param V:           Potential
     :return:            Returns the representation of the H operator with given V
     """
@@ -75,24 +75,44 @@ def H(X, Y, xyF, xyS, nm, nmIndexes, V):
 
     return hMatrix
 
+def calculateState(o, nmIndexes, weights):
+    """
+    This functions calculates the o'th bound state
+    :param o:               order of the bound state
+    :param nmIndexes:       (n,m) indexes of the state basis
+    :param weights:         coefficients for each state function
+    :return:                returns the square of the module of the eigen state
+    """
+    state = 0. + 1j*sp.zeros(X.shape)
+    for i in range(nm):
+       state += weights[i, o] * eigenState(X, Y, nmIndexes[i][0], nmIndexes[i][1], xyMax)
+    state = abs(state)**2
+    return state
+
+
 if __name__ == '__main__':
 
-    mPath = 'matrix.npy'
+    # Some parameters maybe be changed but if so, please delete all the matrices in the matrices folder and recalculate them
+    # These parameters are flagged.
 
     # Defining grid
+    # Don't change please!
     nPoints = 2**8
-    xyMax = 3.0
+    xyMax = 8.0
     xySpace = sp.linspace(0.0, xyMax, nPoints)
     X, Y = sp.meshgrid(xySpace, xySpace)
     spacing = xySpace[1] - xySpace[0]
 
     # Defining problem conditions
+    # Don't change b please!
+    V0 = 100.0
     delta = 0.0
+    # Don't change b please!
     b = 1.0
     a = (1.0 + delta)
-    V0 = 100.0
 
     # Defining state functions max indexes
+    # Don't change this please!!!
     N = 15
     M = 15
     # Calculating the indexes of the state functions that will be used
@@ -102,33 +122,51 @@ if __name__ == '__main__':
         for m in range(1, M+1):
             nmIndexes.append((n,m))
 
+    # Calculations begin here
+
     # Defining the potential
     V = potV(X, Y, xyMax, a, b, V0)
 
     # Creating or loading operator matrix
-    # start = time.time()
-    # print 'Creating operator matrix. Sit back, this may take a while :)'
-    # M = H(X, Y, xyMax, spacing, nm, nmIndexes, V)
-    # sp.save(mPath, M)
-    # print 'Matrix ready'
-    # print 'Took ' + str(time.time() - start) + ' seconds!'
-    M = sp.load(mPath)
+    if (platform.system() == 'Windows'):
+        mPath = 'Operator_Matrices\\delta_' + str(delta) + 'V0_' + str(V0) + '.npy'
+    else:
+        mPath = 'Operator_Matrices/delta_' + str(delta) + 'V0_' + str(V0) + '.npy'
+
+    try:
+        M = sp.load(mPath)
+        print 'Matrix will be loaded'
+    except:
+        start = time.time()
+        print 'Creating operator matrix. Sit back, this may take a while :)'
+        M = H(X, Y, xyMax, spacing, nm, nmIndexes, V)
+        sp.save(mPath, M)
+        print 'Matrix ready'
+        print 'Took ' + str(time.time() - start) + ' seconds!'
 
     # Calculating eigen energies and states
     # values has the energy values
     # weights has the weight for each state on that energy
-    # Could be used to represent the state by summing each state with respective weight
+    # used to represent the state by summing each state with respective weight
     values, weights = linalg.eig(M)
-    values = values[values.argsort()]
-    weights = weights[values.argsort()]
+    indexes = values.argsort()
+    values = values[indexes]
+    weights = weights[:, indexes]
 
-    state = 0. + 1j*sp.zeros(X.shape)
-    for i in range(nm):
-       state += weights[i, 0] * eigenState(X, Y, nmIndexes[i][0], nmIndexes[i][1], xyMax)
-    state = abs(state)**2
-    levels1 = sp.linspace(state.min(), state.max(), 1000)
-
-    pl.figure()
-    pl.contourf(X,Y, state, levels = levels1)
+    # Calculating and plotting first state
+    s1 = calculateState(0, nmIndexes, weights)
+    levels1 = sp.linspace(s1.min(), s1.max(), 1000)
+    pl.figure('First State')
+    pl.contourf(X,Y, s1, levels = levels1)
     pl.colorbar()
+    pl.contour(X, Y, V)
+
+    # Calculating and plotting second state
+    s2 = calculateState(1, nmIndexes, weights)
+    levels2 = sp.linspace(s2.min(), s2.max(), 1000)
+    pl.figure('Second State')
+    pl.contourf(X,Y, s2, levels = levels2)
+    pl.colorbar()
+    pl.contour(X, Y, V)
+
     pl.show()
