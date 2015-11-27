5e5209df48985b5921ab29ad76aa0170e1737959
JoaoCosta94 <jpcc0394@gmail.com>
2015-11-27 00:38:44 +0000

    added cenas

--- HomeWork3/HW3_1c2.py
+++ HomeWork3/HW3_1c2.py
@@ -2,8 +2,9 @@ __author__ = 'JoaoCosta'
 
 import scipy as sp
 import pylab as pl
-from scipy import sum
+from scipy import sum, linalg
 import time
+import pylab as pl
 
 def potV(X, Y, xyF, a, b, V0):
     Z = sp.zeros(X.shape)
@@ -15,6 +16,15 @@ def eigenValues(n, m, xyF):
     return (n*sp.pi/xyF)**2 + (m*sp.pi/xyF)**2
 
 def eigenState(X, Y, n, m, xyF):
+    """
+    This function calculates the eigen state using a fouriers series
+    :param X:       X points
+    :param Y:       Y points
+    :param n:       n index
+    :param m:       m index
+    :param xyF:     box limit
+    :return:        Eigen state as a fourier series
+    """
     return sp.sin((n*sp.pi / xyF) * X) * sp.sin((m*sp.pi / xyF) * Y) * sp.sqrt(4.0 / (xyF**2))
 
 def s1_V_s2(X, Y, xyF, xyS, V, n1, m1, n2, m2):
@@ -36,24 +46,17 @@ def s1_V_s2(X, Y, xyF, xyS, V, n1, m1, n2, m2):
     state2 = eigenState(X, Y, n2, m2, xyF)
     return sum(state1 * V * state2) * xyS**2
 
-def H(X, Y, xyF, xyS, N, M, V):
+def H(X, Y, xyF, xyS, nm, nmIndexes, V):
     """
     This function creates a matrix of the equivalent application of a potential to a free particle,
     like a perturbation theory calculating the 'overlap' of N & M states of X & Y directions
-    :param xyF: External square box limit
-    :param xyS: Grid points spacing
-    :param N:   Limit N of state functions
-    :param M:   Limit M of state functions
-    :param V:   Potential
-    :return:    Returns the representation of the H operator with given V
+    :param xyF:         External square box limit
+    :param xyS:         Grid points spacing
+    :param nm:          Total of states used
+    :param nmIndexes:   (n,m) indexes of the states
+    :param V:           Potential
+    :return:            Returns the representation of the H operator with given V
     """
-
-    # Calculating the indexes of the state functions that will be used
-    nm = N * M
-    nmIndexes = []
-    for n in range(1, N+1):
-        for m in range(1, M+1):
-            nmIndexes.append((n,m))
 
     hMatrix = sp.zeros((nm, nm), dtype = float)
 
@@ -92,18 +95,40 @@ if __name__ == '__main__':
     # Defining state functions max indexes
     N = 15
     M = 15
+    # Calculating the indexes of the state functions that will be used
+    nm = N * M
+    nmIndexes = []
+    for n in range(1, N+1):
+        for m in range(1, M+1):
+            nmIndexes.append((n,m))
 
     # Defining the potential
     V = potV(X, Y, xyMax, a, b, V0)
 
-    # # Creating operator matrix
+    # Creating or loading operator matrix
     # start = time.time()
     # print 'Creating operator matrix. Sit back, this may take a while :)'
-    # M = H(X, Y, xyMax, spacing, N, M, V)
+    # M = H(X, Y, xyMax, spacing, nm, nmIndexes, V)
     # sp.save(mPath, M)
     # print 'Matrix ready'
-    # print 'Took ' + time.time() - start + ' seconds!'
-
+    # print 'Took ' + str(time.time() - start) + ' seconds!'
     M = sp.load(mPath)
 
-    
+    # Calculating eigen energies and states
+    # values has the energy values
+    # weights has the weight for each state on that energy
+    # Could be used to represent the state by summing each state with respective weight
+    values, weights = linalg.eig(M)
+    values = values[values.argsort()]
+    weights = weights[values.argsort()]
+
+    state = 0. + 1j*sp.zeros(X.shape)
+    for i in range(nm):
+       state += weights[i, 0] * eigenState(X, Y, nmIndexes[i][0], nmIndexes[i][1], xyMax)
+    state = abs(state)**2
+    levels1 = sp.linspace(state.min(), state.max(), 1000)
+
+    pl.figure()
+    pl.contourf(X,Y, state, levels = levels1)
+    pl.colorbar()
+    pl.show()
