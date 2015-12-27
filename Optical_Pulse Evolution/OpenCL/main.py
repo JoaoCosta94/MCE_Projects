ef5c9abe473753ac36d17285e81cfcc4edccb1b7
JoaoCosta94 <jpcc0394@gmail.com>
2015-12-27 13:06:51 +0000

    Fixes on kernel multiplication Added OpenCL source to .gitignore

--- Optical_Pulse Evolution/OpenCL/main.py
+++ Optical_Pulse Evolution/OpenCL/main.py
@@ -9,15 +9,16 @@ def initial_state(N):
     """
     This function generates the initial state of the N atoms
     """
-    p11 = sp.random.random_sample(N) + 1j*sp.random.random_sample(N) #
-    p22 = sp.random.random_sample(N) + 1j*sp.random.random_sample(N) #
-    p33 = sp.random.random_sample(N) + 1j*sp.random.random_sample(N) # Creation of initial states of the 3 state atoms
-    p21 = sp.random.random_sample(N) + 1j*sp.random.random_sample(N) #
-    p31 = sp.random.random_sample(N) + 1j*sp.random.random_sample(N) #
-    p32 = sp.random.random_sample(N) + 1j*sp.random.random_sample(N) #
+    # TODO: Change pxy to values that make sense (p11+p22+p33=1)
+    p11 = sp.random.random_sample(N) #
+    p22 = sp.random.random_sample(N) #
+    p33 = sp.random.random_sample(N) # Creation of initial states of the 3 state atoms
+    p21 = sp.random.random_sample(N) #
+    p31 = sp.random.random_sample(N) #
+    p32 = sp.random.random_sample(N) #
     return p11, p22, p33, p21, p31, p32
 
-def device_code(N, dx, dt, P0, DELTA, GAMA, OC):
+def device_code(N, dx, dt, P0, DELTA, GAMA):
     """
     This function generates the source code for the device solver
     """
@@ -29,7 +30,6 @@ def device_code(N, dx, dt, P0, DELTA, GAMA, OC):
     constants += "constant float P0=" + str(P0) + "; \n"
     constants += "constant float DELTA=" + str(DELTA) + "; \n"
     constants += "constant float GAMA=" + str(GAMA) + "; \n"
-    constants += "constant float OC=" + str(OC) + "; \n"
     f1 = open("precode.cl", "r")
     f2 = open("kernel.cl", "r")
     f3 = open("source.cl",'w+')
@@ -40,53 +40,57 @@ def device_code(N, dx, dt, P0, DELTA, GAMA, OC):
     f2.close()
     f3.close()
 
-def device_allocate(ctx, MF, x_h, A_h):
+def device_allocate(ctx, MF, X_h, p_h, A_h, OC_h):
     """
     This function allocates memory on the device
     """
-    x_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=x_h)
+    X_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=X_h)
+    p_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=p_h)
     A_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=A_h)
-    k_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=sp.empty_like(x_h))
-    xs_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=x_h)
-    xm_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=x_h)
-    return x_d, A_d, k_d, xs_d, xm_d
+    OC_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=OC_h)
+    k_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=sp.empty_like(p_h))
+    ps_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=sp.empty_like(p_h))
+    pm_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=sp.empty_like(p_h))
+    return X_d, p_d, A_d, OC_d, k_d, ps_d, pm_d
 
 if __name__ == "__main__":
 
     # Definition of problem parameters
 
     # Grid parameters
-    gWidth = 10000 # atom grid width
+    gWidth = 100 # atom grid width
     dx = sp.float32(0.01) # atom grid spacing
 
     # Time parameters
     tInterval = sp.float32(10.0)
     dt = sp.float32(0.01)
 
-    # State density parameters
-    P0 = sp.float32(1.0)
-    GAMA = sp.float32(1.0)
-    OC = sp.float32(1.0)
-    DELTA = sp.float32(1.0)
-
-    # Envelope parameters
-    a = sp.float32(1.0)
-    b = sp.float32(1.0)
-
     # Generating grids
     X_h = sp.arange(0.0, gWidth, dx).astype(sp.float32)
     T_h = sp.arange(dt, tInterval, dt).astype(sp.float32)
     N = len(X_h)
 
+    # State density parameters
+    P0 = sp.float32(1.0)
+    GAMA = sp.float32(1.0)
+    DELTA = sp.float32(1.0)
+    # TODO: Change OC to values that make sense
+    OC_h = sp.ones(X_h.shape).astype(sp.float32)
+
+    # Envelope parameters
+    a = sp.float32(1.0)
+    b = sp.float32(1.0)
+
     # Generating initial states density
     p11_h, p22_h, p33_h, p21_h, p31_h, p32_h = initial_state(N)
-    p_h = sp.array([p11_h, p22_h, p33_h, p21_h, p31_h, p32_h]).T.astype(sp.complex64)
+    p_h = sp.array([p11_h, p22_h, p33_h, p21_h, p31_h, p32_h]).T.astype(complex)
 
     # Generating initial envelope status
-    A_h = 100.0*(sp.random.random_sample(N) + 1j*sp.random.random_sample(N)).astype(sp.complex64)
+    # TODO: Change A to values that make sense
+    A_h = 100.0*(sp.random.random_sample(N) + 1j*sp.random.random_sample(N)).astype(complex)
 
     # Preparing GPU code
-    device_code(N, dx, dt, P0, DELTA, GAMA, OC)
+    device_code(N, dx, dt, P0, DELTA, GAMA)
 
     # Initialization of the device and workspace
     ctx = cl.create_some_context()
@@ -94,7 +98,7 @@ if __name__ == "__main__":
     MF = cl.mem_flags
 
     # Allocating gpu variables
-    p_d, A_d, k_d, ps_d, pm_d= device_allocate(ctx, MF, p_h, A_h)
+    X_d, p_d, A_d, OC_d, k_d, ps_d, pm_d= device_allocate(ctx, MF, X_h, p_h, A_h, OC_h)
     W = sp.uint32(6) # The row width to compute the index inside the kernel
 
     # Loading the source
@@ -109,7 +113,7 @@ if __name__ == "__main__":
     for t in T_h:
         # print t / tInterval * 100
         # Evolve State
-        evolveSate = prg.RK4Step(queue, (N,), None, p_d, A_d, k_d, ps_d, pm_d, W, t)
+        evolveSate = prg.RK4Step(queue, (N,), None, p_d, A_d, OC_d, k_d, ps_d, pm_d, W, t)
         evolveSate.wait()
 
     tCalc = time.time() - start
